1546364b626f86d4750b0343fc4413fea3360bf7
pistachiyoda <fujisawamai21@gmail.com>
2021-03-26 19:00:14 +0900

    normed

--- ft_printf.c
+++ ft_printf.c
@@ -1,16 +1,27 @@
-#include "ft_printf.h"
-#include "stdio.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fmai <fmai@student.42tokyo.jp>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/26 17:53:05 by fmai              #+#    #+#             */
+/*   Updated: 2021/03/26 18:57:18 by fmai             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-int ft_printf(const char *fmt, ...)
+#include "ft_printf.h"
+
+int			ft_printf(const char *fmt, ...)
 {
-	int output_count;
+	int		output_count;
 	va_list	ap;
-    char *current;
+	char	*current;
 
 	output_count = 0;
 	current = (char *)fmt;
 	va_start(ap, fmt);
-	while(*current)
+	while (*current)
 	{
 		if (*current == '%')
 			output_count += ft_proc_percent(&current, ap);
@@ -20,11 +31,11 @@ int ft_printf(const char *fmt, ...)
 			output_count++;
 		}
 		current++;
-	} 
+	}
 	return (output_count);
 }
 
-t_format init_format()
+t_format	init_format(void)
 {
 	t_format format;
 
@@ -34,17 +45,16 @@ t_format init_format()
 	format.dot_only = 0;
 	format.precision = -1;
 	format.type = '\0';
-
 	return (format);
 }
 
-int ft_proc_percent(char **fmt, va_list ap)
+int			ft_proc_percent(char **fmt, va_list ap)
 {
-	t_format format;
-	int space;
-	
+	t_format	format;
+	int			space;
+
 	format = init_format();
-    (*fmt)++;
+	(*fmt)++;
 	space = proc_spaces(fmt);
 	proc_flags(fmt, &format);
 	proc_field(fmt, &format, ap);
@@ -53,21 +63,21 @@ int ft_proc_percent(char **fmt, va_list ap)
 	return (ft_output(format, ap) + space);
 }
 
-int	ft_output(t_format format, va_list ap)
+int			ft_output(t_format format, va_list ap)
 {
 	if (format.type == 's')
 		return (output_string(format, ap));
 	else if (format.type == 'd' || format.type == 'i')
-		return output_integer(format, ap);
+		return (output_integer(format, ap));
 	else if (format.type == 'u')
-		return output_u_integer(format, ap);
+		return (output_u_integer(format, ap));
 	else if (format.type == 'c')
-		return ft_output_char(format, ap);
+		return (ft_output_char(format, ap));
 	else if (format.type == 'p')
 		output_pointer(format, ap);
 	else if (format.type == 'x' || format.type == 'X')
 		return (output_hexadecimal(format, ap));
 	else if (format.type == '%')
 		return (output_percent(format));
-	return 0;
+	return (0);
 }
