44220e7b8dbc54634f659e2a54cd7b562cd14c11
pistachiyoda <fujisawamai21@gmail.com>
2021-03-30 16:41:14 +0900

    修正

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: fmai <fmai@student.42tokyo.jp>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/26 19:17:28 by fmai              #+#    #+#             */
-/*   Updated: 2021/03/27 10:33:33 by fmai             ###   ########.fr       */
+/*   Updated: 2021/03/30 15:50:42 by fmai             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 # include <stdarg.h>
 
 int				ft_printf(const char *fmt, ...);
+int				ft_proc_percent(char **fmt, va_list ap);
 typedef struct	s_format
 {
 	int			flag_minus;
@@ -25,32 +26,43 @@ typedef struct	s_format
 	int			precision;
 	char		type;
 }				t_format;
-int				ft_isdigit(int c);
-int				ft_atoi(const char *str);
-char			*ft_strchr(const char *s, int c);
-int				ft_proc_percent(char **fmt, va_list ap);
-void			ft_putchar(char c);
-int				ft_isalpha(int c);
-char			*ft_strchr(const char *s, int c);
+t_format		init_format(void);
 int				ft_output(t_format format, va_list ap);
-int				ft_output_char(t_format format, va_list ap);
-int				output_spaces(int num);
-void			ft_putstr(char *s);
-void			ft_putnbr(int n);
 void			proc_flags(char **fmt, t_format *format);
 void			proc_field(char **fmt, t_format *format, va_list ap);
 void			proc_precision(char **fmt, t_format *format, va_list ap);
-int				output_zeros(int num);
 int				output_string(t_format format, va_list ap);
-size_t			ft_strlen(const char *str);
-void			ft_putstr_n(char *s, int len);
-int				output_integer(t_format format, va_list ap);
+int				max_length(int input_str_len, int precision);
+int				calc_decimal_digit(int decimal);
+int				proc_integer_total_digit(
+	t_format format, int minus_digit, int decimal_digit);
+int				proc_integer_zero_digit(
+	t_format format, int minus_digit, int decimal_digit, int total_digit);
+void			putnbr_without_sign(int input_num);
+int				calc_u_decimal_digit(unsigned int decimal);
+int				proc_u_integer_total_digit(t_format format, int decimal_digit);
+int				proc_u_integer_zero_digit(
+	t_format format, int decimal_digit, int total_digit);
+int				ft_output_char(t_format format, va_list ap);
+int				output_pointer(t_format format, va_list ap);
 int				output_hexadecimal(t_format format, va_list ap);
 int				print_hex(
 	t_format format, unsigned int decimal, int hex_digit, char *hex_num);
 int				print_pointer_hex(
 	t_format format, unsigned long long decimal, int digit, char *hex_num);
-int				output_pointer(t_format format, va_list ap);
+int				ft_isdigit(int c);
+int				ft_atoi(const char *str);
+char			*ft_strchr(const char *s, int c);
+void			ft_putchar(char c);
+int				ft_isalpha(int c);
+char			*ft_strchr(const char *s, int c);
+int				output_spaces(int num);
+void			ft_putstr(char *s);
+void			ft_putnbr(int n);
+int				output_zeros(int num);
+size_t			ft_strlen(const char *str);
+void			ft_putstr_n(char *s, int len);
+int				output_integer(t_format format, va_list ap);
 int				proc_total_digit(t_format format, int hex_digit);
 int				proc_zero_digit(
 	t_format format, int total_digit, int hex_digit);
@@ -62,9 +74,5 @@ int				max(int a, int b);
 int				output_percent(t_format format);
 int				output_u_integer(t_format format, va_list ap);
 int				calc_u_decimal_digit(unsigned int decimal);
-int				proc_u_integer_total_digit(
-	t_format format, unsigned int input_num);
-int				proc_u_integer_zero_digit(
-	t_format format, unsigned int input_num);
 void			ft_putunbr(unsigned int n);
 #endif
